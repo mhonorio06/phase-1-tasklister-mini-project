a6fa0a70d6251c39087463d0c3f8789e7e24614c
Manny <mhfernan35277@gmail.com>
2023-08-04 12:44:32 -0700

    finsh tasklister lab

--- src/index.js
+++ src/index.js
@@ -1,3 +1,26 @@
 document.addEventListener("DOMContentLoaded", () => {
   // your code here
-});
+  const taskForm = document.querySelector("#create-task-form")
+  const taskList = document.querySelector("#tasks")
+  taskForm.addEventListener("submit", function (e) {
+  e.preventDefault()
+  const newTask = document.querySelector("#new-task-description").value;
+  makeTasks(newTask)
+  taskForm.reset()
+  })
+})
+  function makeTasks (tasks) {
+    let p = document.createElement("p")
+    let btn = document.createElement("button")
+    btn.addEventListener("click", deleteTask)
+    p.textContent = `${tasks} `
+    btn.textContent = "x"
+    p.appendChild(btn)
+    console.log(p)
+    document.querySelector("#tasks").appendChild(p)
+  }
+  function deleteTask (e){
+    e.target.parentNode.remove()
+  }
+
+
